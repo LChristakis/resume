505f3914d8a974b2e5660577353dd84f5447bcb9
Luke Christakis <lchristakis@gmail.com>
2018-02-10 10:06:28 -0500

    Got template working using Babel

--- js/Resume.js
+++ js/Resume.js
@@ -1,5 +1,37 @@
-var LChristakisResume;
+/** @jsx h */
+
+let LChristakisResumeJSON = {};
 XHR('/LChristakisResume.json',function(data){
-    LChristakisResume = JSON.parse(data);
-    console.log(data);
+    LChristakisResumeJSON = JSON.parse(data);
+    let vdom = (
+        <div id="foo">
+            <ul>{ mapSkills(LChristakisResumeJSON.skillsets) }</ul>
+        </div>
+    );
+
+    document.body.appendChild(render(vdom));
 });
+
+function mapSkills(skillsets) {
+	return skillsets.map( function(skillset){ 
+		var title = skillset.title;
+		var skills = skillset.skills.join(',');
+		return <li><b>{title}:</b>{skills} </li>;
+	});
+}
+
+
+/** Render Virtual DOM to the real DOM */
+function render(vnode) {
+    if (typeof vnode==='string') return document.createTextNode(vnode);
+    let n = document.createElement(vnode.nodeName);
+    Object.keys(vnode.attributes || {}).forEach( k => n.setAttribute(k, vnode.attributes[k]) );
+    (vnode.children || []).forEach( c => n.appendChild(render(c)) );
+    return n;
+}
+
+/** hyperscript generator, gets called by transpiled JSX */
+function h(nodeName, attributes, ...args) {
+    let children = args.length ? [].concat(...args) : null;
+    return { nodeName, attributes, children };
+}
